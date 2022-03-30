e96570e0dbfe536fc67eff05e4137d215cfe3d35
pritoo <preetmangal@gmail.com>
2022-03-30 16:15:45 +0530

    Update office.js

--- controllers/office.js
+++ controllers/office.js
@@ -1,27 +1,42 @@
-import Employee from "../models/office.js"
+import EmployeeData from "../models/office.js"
 //const Employee =require('../models/office') 
 export const getEmployees = async (req,res) => {
    // res.send('Router is working')
 try {
-    const allEmployees = await Employee.find();
-console.log( "allEmployees")
-    res.status(200).json(allEmployees);
+    const allEmployees = await EmployeeData.find();
+//console.log( "allEmployees")
+   return res.status(200).json(allEmployees);
 } catch (error) {
-    res.status(404).json({message:error.message});
+   return res.status(404).json({message:error.message});
 }
 }
+
 
 export const createEmployees = async (req,res) => {
     //res.send('Router is working')
-    //const employee = req.body;
-    const employee ="test";
-    console.log(employee)
-    // const newEmployees = new Employee(employee)
-    // try {
-    //    await newEmployees.save()
+    const {employee} = req.body;
+     //const employee ="test";
+     console.log(employee)
+     //console.log(req.body)
+    const newEmployees = new EmployeeData(employee)
+    try {
+       await newEmployees.save()
 
-    //    res.status(201).json(newEmployees);
-    // } catch (error) {
-    //     res.status(409).json({message:error.message});  
-    // }
+       res.status(201).json(newEmployees);
+    } catch (error) {
+        res.status(409).json({message:error.message});  
+    }
+}
+
+
+export const deleteEmployee = async (req,res) => {
+   
+  const id=req.params.id;
+
+   try {
+      await EmployeeData.findByIdAndRemove(id).exec()
+      res.send(' successfully delete')
+   } catch (error) {
+       console.log(error)
+   }
 }
